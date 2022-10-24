a9268f040c5813c75e5663a05a303fb6293fefd2
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-10-24 12:27:40 -0700

    add output management

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import './style.css';
 import myName from './myName';
 import Icon from './corgi-icon.png';
 import Data from './data.xml';
-import Notes from './data.csv'
+import Notes from './data.csv';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   // element.textContent = myName('Raymart');
   element.classList.add('hello');
+
+  btn.innerHTML = 'Clickme and check the console!';
+  btn.onclick = printMe;
+  
+  element.appendChild(btn);
 
   const corgi = new Image();
   corgi.src = Icon;
