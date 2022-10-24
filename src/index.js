b84d254a0b3db2a0981cf169ff96627f48ad582f
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-10-24 11:59:57 -0700

    load images using webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './style.css';
 import myName from './myName';
+import Icon from './corgi-icon.png';
 
 function component() {
   const element = document.createElement('div');
@@ -8,6 +9,11 @@ function component() {
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   // element.textContent = myName('Raymart');
   element.classList.add('hello');
+
+  const corgi = new Image();
+  corgi.src = Icon;
+
+  element.appendChild(corgi);
 
   return element;
 }
